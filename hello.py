068b436652827cf994eb08adce34bc5cffcf9ff6
shariful <mshariful@users.noreply.github.com>
2026-05-27 11:14:39 +0000

    My first commit!

---
+++ hello.py
@@ -0,0 +1,19 @@
+def more_hello():
+    # import pdb;pdb.set_trace()
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+if __name__ == "__main__":
+    more_hello()
+    more_goodbye()
+
+# This is nonsense code that generates a warning
+# var = 1
+# var = var
+
+# Bad syntax
+# foo =
